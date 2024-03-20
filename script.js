6c88161a59d3e7a18eabf856924938e80ec1c1ca
Joydeep Sarkar <138491246+joydeepsarkar99@users.noreply.github.com>
2024-03-20 18:53:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+let inputRadius = document.querySelector("#radius")
+let outputVolume = document.querySelector("#volume")
+
 function volume_sphere() {
-    //Write your code here
-  
+    let r = +inputRadius.value
+	if(r >= 0 && typeof(r) === "number"){
+		let ans = ((4/3)*(Math.PI)*(r**3)).toFixed(4)
+		outputVolume.value = ans
+	}
+	else {
+        outputVolume.value = "NaN";
+    }
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
